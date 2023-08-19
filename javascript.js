d5ddba2979457c64de82e2ae4f8c1492d7dbd302
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-19 17:19:13 -0400

    Implement grid with hover effect
    Added mouseover event with function that changes the color of each cell
    in the grid

--- javascript.js
+++ javascript.js
@@ -1,22 +1,29 @@
 
-
-let body = document.querySelector("body");
-
+let gridContainer =document.getElementById("gridContainer")
 
 
 for (let i = 0; i < 16; i++) {
 let row = document.createElement("div")
-body.appendChild(row)
+gridContainer.appendChild(row)
 row.className = "row"
 
+
 for (let e = 0; e < 16; e++) {
+    gridContainer =document.getElementById("gridContainer")
+    function colorChange (element,color) {
+        element.style.backgroundColor = color;
+    }
+    
+
+
+
  let column = document.createElement("div")
  row.appendChild(column)
  column.className = "column";
+ column.addEventListener("mouseover", () => colorChange(column,"purple") )
 }
 
 }
-
 
 
 
