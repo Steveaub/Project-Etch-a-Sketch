d383a367da8dc37aa8368f2fa4dd04c9fec473ca
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-20 00:29:08 -0400

    Implement dynamic grid generation and color
    - Refactor grid generation into a separate function for modularity.
    - Introduce clearGrid function to remove previous grids when generating
      a new one based on user input.
    - Implement random color generation for the hover effect on grid cells.
    - Add validation for user input to ensure the grid size is within limits.

--- javascript.js
+++ javascript.js
@@ -1,31 +1,57 @@
 
 let gridContainer =document.getElementById("gridContainer")
 
-
-for (let i = 0; i < 16; i++) {
-let row = document.createElement("div")
-gridContainer.appendChild(row)
-row.className = "row"
+function colorChange (element,color) {
+    element.style.backgroundColor = color;
+}
 
 
-for (let e = 0; e < 16; e++) {
-    gridContainer =document.getElementById("gridContainer")
-    function colorChange (element,color) {
-        element.style.backgroundColor = color;
-    }
-    
+function generateGrid(size) {
+
+    for (let i = 0; i < size; i++) {
+let row = document.createElement("div");
+gridContainer.appendChild(row);
+row.className = "row";
 
 
-
- let column = document.createElement("div")
- row.appendChild(column)
+for (let e = 0; e < size; e++) {
+let column = document.createElement("div");
+ row.appendChild(column);
  column.className = "column";
- column.addEventListener("mouseover", () => colorChange(column,"purple") )
+ column.addEventListener("mouseover", () => colorChange(column,randomRbg ()) );
 }
 
 }
 
+}
+
+
+function clearGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+
+let newGrid = document.getElementById("newGrid");
+newGrid.addEventListener("click", function() {
+    let size = parseInt(prompt("What number of squares per side would you like?"), 10);
+    if (isNaN(size) || size < 1 || size > 100) {
+        alert("Please enter a number between 1 and 100.");
+        return;
+    }
+    clearGrid();
+    generateGrid(size);
+});
+
+// Initial grid creation on page load
+generateGrid(16);
 
 
 
+function randomRbg (){
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
 
+    return `rgb(${red} ,${blue} ,${green})`
+  }
